8d9078f339e25b6a1fa7ff5d1398702198d2b1ec
Simonebaca <simobaco99@gmail.com>
2023-03-08 16:28:51 +0100

    add-discountprice

--- main.js
+++ main.js
@@ -11,9 +11,31 @@ console.log(userkm);
 userage = prompt("Please enter your age");
 console.log(userage);
 
-// cosi ottengo il costo del biglietto senza sconti
-let result = userkm * 0.21;
-console.log(result);
-let element = document.getElementById("result_display");
-console.log ();
-element.innerHTML = result;
+
+// moltiplico il prezzo per i numeri di km selezionati dal cliente 
+const priceperKm = 0.21;
+const provisionalPrice = userkm * priceperKm;
+console.log(provisionalPrice);
+let totalPrice = 0;
+//htmlcol
+const priceDisplay = document.getElementById('result_display');
+if (userage < 18) {
+    // applico lo sconto per i minori di 18 anni del 20%
+    const discount20 = provisionalPrice / 100 * 20;
+    totalPrice = provisionalPrice - discount20;
+    console.log(totalPrice);
+    //htmlcol
+    priceDisplay.innerHTML = `&euro; ${totalPrice}`;
+
+} else if (userage >= 65) {
+    // applico lo sconto per gli over= 65 de 40%
+    const discount40 = provisionalPrice / 100 * 40;
+    totalPrice = provisionalPrice - discount40;
+    console.log(totalPrice);
+    //htmlcol
+    priceDisplay.innerHTML = `&euro; ${totalPrice}`;
+} else {
+    totalPrice = provisionalPrice;
+    //htmlcol
+    priceDisplay.innerHTML = `&euro; ${totalPrice}`;
+}
